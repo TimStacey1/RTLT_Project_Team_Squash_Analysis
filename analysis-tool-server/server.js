e3119cd37a021a3b5201741c31a17b3a0ec7682b
Nick <nick.dodig@gmail.com>
2021-05-19 00:03:17 +1000

    Setup server side file structure.

--- analysis-tool-server/server.js
+++ analysis-tool-server/server.js
@@ -3,9 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const router = require('.src/api/routes/index.js');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -19,8 +18,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ analysis-tool-server/src/api/routes/index.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+
+// Import all routes here
+// Connect routes to controllers
+// export as router for server.js
+module.exports = router;
