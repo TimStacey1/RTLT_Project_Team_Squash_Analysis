1192826c6f93359e41d3eb97a4315bc575d13f28
Joshua Young <djry1999@gmail.com>
2021-08-04 12:43:56 +1000

    [MA1:005] API for removing an existing annotation

--- analysis-tool-server/routes/annotate.js
+++ analysis-tool-server/routes/annotate.js
@@ -86,11 +86,34 @@ router.post('/:match_id/:annotation_id/edit', (req, res, next) => {
     components: components
   };
 
+  // find the match using the provided id, find the annotation using
+  // the annotation id and update its contents
   Match.updateOne(
     { _id: matchId, 'annotations._id': annotationId },
     { $set: { 'annotations.$': updatedAnnotation } }
   )
   .then(res.status(200).json('Annotation updated!'))
+  .catch(err => res.status(400).json('Error: ' + err));
+});
+
+// route for removing an existing annotation of a match
+router.post('/:match_id/:annotation_id/remove', (req, res, next) => {
+  const matchId = req.params.match_id;
+  const annotationId = req.params.annotation_id;
+
+  // TODO: validate match id and annotation id
+
+  // add specific queries to be de-structured before the remaining queries
+  // eg: { shot, ...remainingQueries}
+  // const { ...remainingQueries } = req.params.query;
+
+  // find the match using the provided id, find the annotation using
+  // the annotation id and discard it
+  Match.updateOne(
+    { _id: matchId },
+    { $pull: { annotations: { _id: annotationId } } }
+  )
+  .then(res.status(200).json('Annotation removed!'))
   .catch(err => res.status(400).json('Error: ' + err));
 });
 
