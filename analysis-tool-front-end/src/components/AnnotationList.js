27db0f4f5ba8fc0f7162bd709363cf90652f81c5
Joshua Young <djry1999@gmail.com>
2021-08-30 22:43:26 +1000

    Prettier cleanup for front end

--- analysis-tool-front-end/src/components/AnnotationList.js
+++ analysis-tool-front-end/src/components/AnnotationList.js
@@ -17,13 +17,10 @@ export default function AnnotationList(props) {
     endTimeM: 0,
     endTimeS: 0
   });
-  const [checkedState, setCheckedState] = useState(
-    new Array(12).fill(false)
-    );
+  const [checkedState, setCheckedState] = useState(new Array(12).fill(false));
 
-    
   const [modalContent, setModalContent] = useState({});
-  var unique_shots = [...new Set(annotations.map(item => item.shot.id))];
+  var unique_shots = [...new Set(annotations.map((item) => item.shot.id))];
 
   useEffect(() => {
     if (Object.entries(annotationToRemove).length !== 0) {
@@ -55,59 +52,63 @@ export default function AnnotationList(props) {
   }, [annotationToRemove, props]);
 
   const clearFilters = () => {
-    const updatedCheckedState =  new Array(12).fill(false);
+    const updatedCheckedState = new Array(12).fill(false);
     setCheckedState(updatedCheckedState);
     setFilterAnnotations(annotations);
     setFilterTime({
       startTimeM: 0,
       startTimeS: 0,
       endTimeM: 0,
-      endTimeS: 0,
-
-    })
-  }
+      endTimeS: 0
+    });
+  };
 
   const shotFilterChange = (position) => {
-
     const updatedCheckedState = checkedState.map((item, index) =>
       index === position ? !item : item
     );
 
     const indices = updatedCheckedState.reduce(
-  (out, bool, index) => bool ? out.concat(index) : out,
-  []
-)
-  var unique = [];
-  for (var i = 0; i < indices.length; i++) {
-    unique.push(unique_shots[indices[i]]) ;
+      (out, bool, index) => (bool ? out.concat(index) : out),
+      []
+    );
+    var unique = [];
+    for (var i = 0; i < indices.length; i++) {
+      unique.push(unique_shots[indices[i]]);
+    }
 
-  }
+    let newAnnotations = annotations.filter((item) =>
+      unique.includes(item.shot.id)
+    );
 
-  let newAnnotations = annotations.filter(item => unique.includes(item.shot.id));
+    console.log(newAnnotations);
+    setFilterAnnotations(newAnnotations);
 
-  console.log(newAnnotations);
-  setFilterAnnotations(newAnnotations);
+    setCheckedState(updatedCheckedState);
 
-  setCheckedState(updatedCheckedState);
-
-  let checker = checkedState => checkedState.every(v => v === false);
-  if (checker(updatedCheckedState)) {
+    let checker = (checkedState) => checkedState.every((v) => v === false);
+    if (checker(updatedCheckedState)) {
       setFilterAnnotations(annotations);
     }
-};
+  };
 
-  const filterTimeChange = (evt,field) => {
+  const filterTimeChange = (evt, field) => {
     let newAnnotations = filterTime;
     newAnnotations[field] = evt.target.value;
-    const startS = (Number(newAnnotations.startTimeM)*60)+Number(newAnnotations.startTimeS);
-    const endS = (Number(newAnnotations.endTimeM)*60)+Number(newAnnotations.endTimeS);
-    newAnnotations = annotations.filter(item => (item.timestamp >= startS) && item.timestamp <= endS);
+    const startS =
+      Number(newAnnotations.startTimeM) * 60 +
+      Number(newAnnotations.startTimeS);
+    const endS =
+      Number(newAnnotations.endTimeM) * 60 + Number(newAnnotations.endTimeS);
+    newAnnotations = annotations.filter(
+      (item) => item.timestamp >= startS && item.timestamp <= endS
+    );
     setFilterAnnotations(newAnnotations);
-}
+  };
 
-const toggleFilter = () => setFilterShow(!showFilter);
+  const toggleFilter = () => setFilterShow(!showFilter);
 
-const showModal = (shotText, timeText) => {
+  const showModal = (shotText, timeText) => {
     if (shotText && timeText) {
       setModalContent({
         shotText: shotText,
@@ -142,71 +143,119 @@ const showModal = (shotText, timeText) => {
     return hoursDisplay + ':' + minutesDisplay + ':' + secondsDisplay;
   };
 
+  const Filter = () => (
+    <div className="modal" id="modal">
+      <h2 className="text-center">Filter Annotations</h2>
+      <div className="content">
+        <div className="container">
+          <div className="grid grid-cols-12">
+            <div className="col-start-1 col-end-6  border-2 p-2">
+              <h3 className="font-bold text-lg mb-1 text-center">
+                Filter by shot
+              </h3>
+              {unique_shots.map((shot, index) => {
+                return (
+                  <>
+                    <label className="block">
+                      <input
+                        type="checkbox"
+                        class="form-checkbox"
+                        checked={checkedState[index]}
+                        onChange={() => shotFilterChange(index, shot)}
+                      />
+                      <span className="ml-2">{shot}</span>
+                    </label>
+                  </>
+                );
+              })}
+            </div>
 
-  const Filter = () =>
+            <div className="col-start-7 col-end-13 border-2 p-2">
+              <h3 className="font-bold text-lg mb-1 text-center">
+                Filter by time
+              </h3>
+              <h5 className="">Start Time:</h5>
 
-  <div className="modal" id="modal">
-  <h2 className="text-center">Filter Annotations</h2>
-  <div className="content">
-  <div className="container">
-  <div className="grid grid-cols-12">
-  <div className="col-start-1 col-end-6  border-2 p-2">
-        <h3 className="font-bold text-lg mb-1 text-center">Filter by shot</h3>
-  {unique_shots.map((shot, index) => {
-    return (
-      <>
-        <label className="block">
-        <input type="checkbox" class="form-checkbox" checked={checkedState[index]} onChange={() => shotFilterChange(index, shot)} />
-          <span className="ml-2">{shot}</span> 
-        </label>
-      </>)
-   })}
-           </div>
+              <div className="">
+                <label className="inline">
+                  <span className="">Min</span>
 
-   <div className="col-start-7 col-end-13 border-2 p-2">
-          <h3 className="font-bold text-lg mb-1 text-center">Filter by time</h3>
-          <h5 className="">Start Time:</h5>
-        
-        <div className="">
-        <label className="inline">
-        <span className="">Min</span> 
+                  <input
+                    type="number"
+                    min="0"
+                    name="startTimeM"
+                    value={filterTime.startTimeM}
+                    onChange={(event) => filterTimeChange(event, 'startTimeM')}
+                    className="w-1/4 ml-2 form-text border-2 mr-1 pl-1"
+                  />
+                </label>
 
-        <input type="number" min="0" name="startTimeM" value={filterTime.startTimeM} onChange={(event) => filterTimeChange(event, 'startTimeM')} className="w-1/4 ml-2 form-text border-2 mr-1 pl-1" />
-        </label>
+                <label className="inline">
+                  <span className="">Sec</span>
 
-        <label className="inline">
-        <span className="">Sec</span> 
+                  <input
+                    type="number"
+                    max="60"
+                    min="0"
+                    name="startTimeS"
+                    value={filterTime.startTimeS}
+                    onChange={(event) => filterTimeChange(event, 'startTimeS')}
+                    className="w-1/4 ml-2 form-text border-2 pl-1"
+                  />
+                </label>
+              </div>
+              <h5 className="">End Time:</h5>
 
-        <input type="number" max="60" min="0" name="startTimeS" value={filterTime.startTimeS}  onChange={(event) => filterTimeChange(event, 'startTimeS')} className="w-1/4 ml-2 form-text border-2 pl-1"/>
-        </label>
+              <div className="">
+                <label className="inline">
+                  <span className="">Min</span>
+
+                  <input
+                    type="number"
+                    min="0"
+                    name="endTimeM"
+                    value={filterTime.endTimeM}
+                    onChange={(event) => filterTimeChange(event, 'endTimeM')}
+                    className="w-1/4 ml-2 form-text border-2 mr-1 pl-1"
+                  />
+                </label>
+
+                <label className="inline">
+                  <span className="">Sec</span>
+
+                  <input
+                    type="number"
+                    max="60"
+                    name="endTimeS"
+                    value={filterTime.endTimeS}
+                    onChange={(event) => filterTimeChange(event, 'endTimeS')}
+                    className="w-1/4 ml-2 form-text border-2 pl-1"
+                  />
+                </label>
+              </div>
+            </div>
+          </div>
         </div>
-        <h5 className="">End Time:</h5>
-        
-        <div className="">
-        <label className="inline">
-        <span className="">Min</span> 
+      </div>
 
-        <input type="number" min="0" name="endTimeM" value={filterTime.endTimeM} onChange={(event) => filterTimeChange(event, 'endTimeM')} className="w-1/4 ml-2 form-text border-2 mr-1 pl-1" />
-        </label>
-
-        <label className="inline">
-        <span className="">Sec</span> 
-
-        <input type="number" max="60" name="endTimeS" value={filterTime.endTimeS} onChange={(event) => filterTimeChange(event, 'endTimeS')} className="w-1/4 ml-2 form-text border-2 pl-1"/>
-        </label>
-
-        </div>
+      <div className="actions">
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mr-2"
+          onClick={clearFilters}
+        >
+          {' '}
+          Clear Filters{' '}
+        </button>
+        <button
+          className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4"
+          onClick={toggleFilter}
+        >
+          {' '}
+          Cancel{' '}
+        </button>
+      </div>
     </div>
-    </div>
-    </div>
-   </div>
-
-   <div className="actions">
-   <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mr-2" onClick={clearFilters}> Clear Filters </button>
-   <button className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4" onClick={toggleFilter}> Cancel </button>
-
- </div>
-</div>;
+  );
 
   return (
     <>
@@ -267,23 +316,23 @@ const showModal = (shotText, timeText) => {
                         {convertSecondsToMS(annotation.timestamp)}
                       </button>
                     </td>
-
                   </tr>
                 </>
               );
             })}
           </tbody>
-
-
         </table>
-
       </div>
 
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 w-full" onClick={toggleFilter}> Filter  </button>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 w-full"
+        onClick={toggleFilter}
+      >
+        {' '}
+        Filter{' '}
+      </button>
 
-
-{showFilter ? <Filter /> : null}
-
+      {showFilter ? <Filter /> : null}
     </>
   );
 }
